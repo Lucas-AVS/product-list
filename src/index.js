bb57d63447e777f9bdeb36cd83492040a1b2db62
Lucas-AVS <lucas.alencar31@gmail.com>
2025-01-07 18:24:02 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+const content = document.querySelector(".product-container");
+
+const test = document.createElement("h1");
+test.textContent = "This is a test";
+
+content.appendChild(test);
