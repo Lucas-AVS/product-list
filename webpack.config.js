9fd9e9b46ac6c9f135c9aa82c0e3223c915af6cf
Lucas-AVS <lucas.alencar31@gmail.com>
2025-01-12 22:35:55 -0300

    chore: webpack config to correct img formats

--- webpack.config.js
+++ webpack.config.js
@@ -14,12 +14,12 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
+        test: /\.(png|jpg|jpeg|gif)$/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
     ],
   },
